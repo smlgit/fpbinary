f3b3b04f22255e7262bea0d82f7733da2af4ebbe
smlgit <smlgit@protonmail.com>
2022-11-13 14:38:52 +1300

    Make release script die if twine not installed.

--- release/lib/pypi.py
+++ release/lib/pypi.py
@@ -9,7 +9,8 @@ def upload_to_pypi_server(password, files_dir, server='testpypi'):
     result = subprocess.run(['python', '-m', 'twine', 'upload', '--repository',
                              '{}'.format(server), '-u', '__token__', '-p',
                              '{}'.format(password), '--verbose',
-                             '{}/*'.format(files_dir.rstrip('/'))])
+                             '{}/*'.format(files_dir.rstrip('/'))],
+                            check=True)
 
     return result == 0
 
